73e4b47898ad91d57dd1b803aae9e71cf7da7b44
luddelewis <74183690+luddelewis@users.noreply.github.com>
2021-05-26 23:06:19 +0200

    Added formchecking, not done yet

--- js/form.js
+++ js/form.js
@@ -1,1 +1,6 @@
-//Formcheck js
+//Form js
+function formcheck(){
+	//Changes the class of the form to was-validated to make it green or red depending on if it was filled in
+	document.getElementById('form').setAttribute('class', 'was-validated two_col image_container');
+	//do something with a hidden class for annat land saken
+}
--- js/responsiveness.js
+++ js/responsiveness.js
@@ -3,9 +3,9 @@
 //function for making the yt vid have a fixed aspect ratio
 function responsiveVid(){
 	//Gets the width of the element with id 'video'
-	var width = document.getElementById('video').getBoundingClientRect().width;
+	var width = document.getElementById('keepAspect').getBoundingClientRect().width;
 	//Calculation for aspect ratio
 	var height = (width/16)*9;
 	//Sets the height to the calculated value
-	document.getElementById('video').style.height = height + "px";
+	document.getElementById('keepAspect').style.height = height + "px";
 }
