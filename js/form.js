c9430f723cb2361746188a50e457399337611a6b
luddelewis <74183690+luddelewis@users.noreply.github.com>
2021-05-27 21:10:40 +0200

    Formchecking pretty much done

--- js/form.js
+++ js/form.js
@@ -2,5 +2,14 @@
 function formcheck(){
 	//Changes the class of the form to was-validated to make it green or red depending on if it was filled in
 	document.getElementById('form').setAttribute('class', 'was-validated two_col image_container');
-	//do something with a hidden class for annat land saken
+}
+function ctzshipCheck(){
+	var ctzshipQuestion = document.getElementById('ctzship');
+	var selectedOption = ctzshipQuestion.options[ctzshipQuestion.selectedIndex].text;
+	console.log(selectedOption);
+	if( selectedOption === "Annat" ){
+	document.getElementById('hiddenInput').setAttribute('class', '');
+	}
+	else
+	document.getElementById('hiddenInput').setAttribute('class', 'hideMe');
 }
